39b1014097c496392010a85113851fa56eb2202f
AndGasper <24193043+AndGasper@users.noreply.github.com>
2018-02-23 06:05:05 -0800

    Removed bin/ and obj/ directories.

---
+++ RedisSampleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+[assembly: AssemblyTitle("Redis Sample Application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Redis Sample Application")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
